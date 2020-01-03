9f33401950ee8a76960ad10c7575d7830db10e58
wael-zoaiter <wael.syr.95@gmail.com>
2020-01-03 20:03:42 +0200

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.205f8392c4f22e427517b21a04a7f39a.js"
+  "/precache-manifest.37aedef36238edd3151cd253928dd7d2.js"
 );
 
 self.addEventListener('message', (event) => {
